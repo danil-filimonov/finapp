fe41388dfcbb14990d3b53fe39ac7ac82e0695f6
DFilimonov <>
2022-07-09 14:46:55 +0700

    chore(): add lints and other libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
 export const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+    return <div className='App'></div>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { App } from './app';
+
 import { store } from './app/store';
-import { App } from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './report-web-vitals';
+
 import './index.css';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={ store }>
+            <App />
+        </Provider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
